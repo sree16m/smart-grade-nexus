fb8e479925c090b280db0349793bafbef6af6197
Srikanth <srikanth.pichika@redcross.org>
2026-02-11 09:17:13 +0530

    chore: remove superseded render-build.sh

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Install Tesseract and its dependencies
-apt-get update && apt-get install -y tesseract-ocr libtesseract-dev poppler-utils
-
-# Install Python Dependencies
-pip install -r requirements.txt
